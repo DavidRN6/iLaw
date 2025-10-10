67c4dd441b152032e97b43de51f5ef12569d25d1
David <yugiohgxd600@gmail.com>
2025-10-09 17:05:24 -0700

    first commit

---
+++ app/layout.js
@@ -0,0 +1,39 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "./prism.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
+
+const inter = Inter({
+  variable: "--font-Inter",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "iLaw AI Chat",
+  description: "Made by David Raoof",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <AppContextProvider>
+        <html lang="en">
+          <head>
+            <link rel="icon" href="/favicon.ico" />
+          </head>
+          <body className={`${inter.className} antialiased`}>
+            <Toaster
+              toastOptions={{
+                success: { style: { background: "black", color: "white" } },
+                error: { style: { background: "black", color: "white" } },
+              }}
+            />
+            {children}
+          </body>
+        </html>
+      </AppContextProvider>
+    </ClerkProvider>
+  );
+}
